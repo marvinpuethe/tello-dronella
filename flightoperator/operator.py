3a9a01f66c104704d78cd55558eaf866edf15251
Marvin Püthe <marvin@marvinpuethe.de>
2019-07-03 01:46:31 +0200

    Add operator for multiflight operations

---
+++ flightoperator/operator.py
@@ -0,0 +1,68 @@
+from drone.tello import Tello
+import os
+from datetime import datetime
+
+
+class Operator:
+
+    def __init__(self):
+        self.swarm = []
+
+        self.path_to_log = 'log'
+        self.start_time = datetime.now().isoformat().replace(':', '-')
+
+    def __del__(self):
+        self.end_connection
+        self.save_log
+
+    def add_drone(self, tello):
+        '''
+        Add the given drone to the swarm
+        '''
+        self.swarm.append(tello)
+
+    def execute_command(self, command):
+        '''
+        Execute the command on each drone
+        '''
+        for tello in self.swarm:
+            # TODO Execute command as thread
+            if not tello.send_command(command).success():
+                tello.send_command('land')
+
+    def register_drone(self, ip, wifi='operator', ap='dronella'):
+        '''
+        Register drone with the given op to the given access point
+        '''
+        tello = Tello(ip)
+        if tello.send_command('ap ' + wifi + ' ' + ap).success():
+            self.add_drone(tello)
+
+    def land_swarm(self):
+        '''
+        Send the land command to all drones
+        '''
+        self.execute_command('land')
+        self.end_connection()
+
+    def end_connection(self):
+        '''
+        End the socket connection of each drone
+        '''
+        for tello in self.swarm:
+            tello.close_connection()
+
+    def save_log(self):
+        '''
+        Print the log for each drone to the stdout
+        '''
+        if not os.path.isdir(self.path_to_log):
+            os.mkdir(self.path_to_log)
+        out = open('log' + os.path.sep + self.start_time + '.txt', 'w')
+
+        for tello in self.swarm:
+            log = tello.log
+            print(tello.tello_sn)
+            for entry in log:
+                print(entry)
+                out.write(str(entry))
