11b57e53b14d5ed9d6bf8226b04b9e358cd976ac
Marvin Püthe <marvin@marvinpuethe.de>
2019-07-03 01:46:46 +0200

    Update main.py to reflect changes in program structure

--- main.py
+++ main.py
@@ -1,47 +1,102 @@
 from drone.tello import Tello
+from flightoperator.operator import Operator
 import sys
-from datetime import datetime
 import time
 import os
-import shutil
 
-start_time = datetime.now().isoformat().replace(':', '-')
-path_to_log = 'log'
 
-file_name = ''
-if len(sys.argv) >= 2:
-    print('Using file %s' % sys.argv[1])
-    file_name = sys.argv[1]
-else:
-    print('No parameter given. Trying to use command.txt')
-    file_name = 'command.txt'
-
-tello = Tello()
-
-# Scripted mode
-if os.path.isfile(file_name):
-
-    print('command.txt found. Executing script')
-    f = open(file_name, "r")
+def execute_commands_from_file(command_file_name):
+    print('Command file found. Executing script')
+    f = open(command_file_name, "r")
     commands = f.readlines()
 
     for command in commands:
-        returncode = 0
         if command != '' and command != '\n':
             command = command.rstrip()
 
-            if command.find('delay') != None:
+            if command.find('delay') != -1:
                 sec = float(command.partition('delay')[2])
                 print('delay %s' % sec)
                 time.sleep(sec)
-                pass
             else:
-                if tello.send_command(command) == None:
-                    break
+                operator.execute_command(command)
 
-# Interactive mode
+
+def get_swarm_file():
+    '''
+    Get the swarm file either from the argument list or as the standard file. If not file is found return None
+    '''
+    # Try to get the swarm file from argument list
+    try:
+        swarm_file = sys.argv[1]
+    # If not try standard file
+    except IndexError:
+        swarm_file = 'swarm.txt'
+
+    # If no parameter was given try standard file
+    return swarm_file
+
+
+def get_command_file():
+    '''
+    Get the command file either from the argument list or as the standard file. If not file is found return None
+    '''
+    # Try to get the command file from argument list
+    try:
+        command_file = sys.argv[2]
+    # If no parameter was given try standard file
+    except IndexError:
+        command_file = 'command.txt'
+    return command_file
+
+
+def get_swarm_ips_from_keyboard():
+
+    swarm_ips = []
+
+    tello_ip = ''
+    while (tello_ip != 'end'):
+        tello_ip = input(
+            'Swarm file not found. Specify drone ips (empty for single mode)\nExit input with \'end\'\n')
+
+        if tello_ip == 'end':
+            break
+
+        if (tello_ip == '' and operator.swarm == None):
+            swarm_ips.append('192.168.10.1')
+            break
+
+        swarm_ips.append(tello_ip)
+
+    return swarm_ips
+
+
+operator = Operator()
+
+# Get drone ip adresses
+swarm_file_name = get_swarm_file()
+if (swarm_file_name == None):
+    swarm_ips = get_swarm_ips_from_keyboard()
 else:
-    print('command.txt does not exist. Starting in interactive mode\nExit with \'end\'')
+    f = open(swarm_file_name, "r")
+    ips = f.readlines()
+
+    swarm_ips = []
+    for ip in ips:
+        swarm_ips.append(ip.rstrip())
+
+# Add drones to swarm
+for drone_ip in swarm_ips:
+    operator.add_drone(Tello(drone_ip))
+
+# Check for command file and execute if found
+command_file_name = get_command_file()
+if os.path.isfile(command_file_name):
+    execute_commands_from_file(command_file_name)
+
+# If no command file has been given start in interactive mode
+else:
+    print('Command file not found. Starting in interactive mode\nExit with \'end\'')
     print('Ready for takeoff... 🚀\nTry typing takeoff:')
 
     while True:
@@ -49,28 +104,17 @@ else:
             command = input()
 
         except KeyboardInterrupt:
-            tello.send_command('land')
+            operator.land_swarm()
             break
 
         if not command:
             break
 
-        if 'end' in command or tello.send_command(command) == None:
-            tello.send_command('land')
+        if 'end' in command:
+            operator.land_swarm()
             break
 
         # Send command to tello drone. If return is None end communication
-        if tello.send_command(command) == None:
-            break
+        operator.execute_command(command)
 
-tello.close_connection()
-
-# Save log to log-path
-if not os.path.isdir(path_to_log):
-    os.mkdir(path_to_log)
-out = open('log' + os.path.sep + start_time + '.txt', 'w')
-
-log = tello.log
-for entry in log:
-    print(entry)
-    out.write(str(entry))
+del operator
